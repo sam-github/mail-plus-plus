b1dcee7be0cab8bfa3545237c1e84477a5e39412
Sam Roberts <vieuxtech@gmail.com>
1999-11-07 16:41:42 +0000

    *** empty log message ***

--- base64.cc
+++ base64.cc
@@ -4,68 +4,10 @@
 
 #include "base64_table.c"
 
-	union B64Chunk
-	{
-	/*
-		// Used to uncover the host bit-order...
-		struct {
-			int a : 1;
-			int b : 1;
-			int c : 1;
-			int d : 1;
-			int e : 1;
-			int f : 1;
-			int g : 1;
-			int h : 1;
-		} b2[3];
-		struct {
-			char	octet[3];
-			char& operator [] (int index)
-			{
-				assert(index >= 0 && index < 3);
-				int i;
-				switch(index)
-				{
-				case 2: i = 0; break;
-				case 1: i = 1; break;
-				case 0: i = 2; break;
-				}
-				return octet[i];
-			}
-		}  b256;
-	*/
-		char b256[3];
-		struct {
-			int	d : 6;
-			int	c : 6;
-			int	b : 6;
-			int	a : 6;
-/*
-			char operator [] (int index)
-			{
-				assert(index >= 0 && index < 4);
-				switch(index)
-				{
-				case 0: return a;
-				case 1: return b;
-				case 2: return c;
-				case 3: return d;
-				}
-				return -1;
-			}
-*/
-		} b64;
-	};
-
-
-/*
 class Base64Encoder
 {
 private:
 public:
-	B64Chunk chunk;
-	int		octet;
-
 	class Buffer
 	{
 	public:
@@ -86,35 +28,10 @@ public:
 	};
 	Buffer buffer;
 
-	const int maxlength; // max number of chars/line
-	int	llength;
-	int octets;
-
-	void Convert()
-	{
-		if(octets == 0)
-			return;
-
-		// write enough b64s to include all the octets
-		int b64s = ((octets * 8) / 6) + (((octets * 8) % 6) ? 1 : 0);
-
-		// print the four b64 encoded chars, perhaps with padding
-		for(int i = 0; i < 4; i++) {
-			if(i < b64s) {
-				char b = chunk.b64[i];
-				char e = base64_encode[b];
-				buffer.Push(e);
-			} else {
-				buffer.Push('=');
-			}
-			llength++;
-			if(llength > maxlength) {
-				buffer.Push('\r');
-				buffer.Push('\n');
-				llength = 0;
-			}
-		}
-	}
+	int		chunk;
+	int		octets;
+	int		maxlength; // max number of chars/line
+	int		llength;
 
 public:
 	Base64Encoder() :
@@ -127,12 +44,48 @@ public:
 	void Push(int c)
 	{
 		if(c == EOF) {
-			Convert();
-		} else {
-			chunk.b256[octets] = (char)c;
-			if(++octets == 3) {
-				Convert();
+			switch(octets) {
+			case 1:
+				buffer.Push(base64_encode[chunk << 4]);
+				buffer.Push('=');
+				buffer.Push('=');
+				break;
+			case 2:
+				buffer.Push(base64_encode[chunk << 2]);
+				buffer.Push('=');
+			case 0:
+				break;
 			}
+
+			buffer.Push('\r');
+			buffer.Push('\n');
+		} else {
+			switch(octets) {
+			case 0:
+				buffer.Push(base64_encode[c >> 2]);
+				chunk = c & 0x3;
+				octets = 1;
+				llength++;
+				break;
+			case 1:
+				buffer.Push(base64_encode[(chunk << 4) | (c >> 4)]);
+				chunk = c & 0xf;
+				octets = 2;
+				llength++;
+				break;
+			case 2:
+				buffer.Push(base64_encode[(chunk << 2) | (c >> 6)]);
+				buffer.Push(base64_encode[c & 0x3f]);
+				octets = 0;
+				llength += 2;
+				break;
+			}
+			if(llength > maxlength) {
+				buffer.Push('\r');
+				buffer.Push('\n');
+				llength = 0;
+			}
+
 		}
 	}
 	int Pop()
@@ -141,7 +94,7 @@ public:
 	}
 
 };
-*/
+
 
 int main()
 {
@@ -150,88 +103,16 @@ int main()
 
 	const void * vp = &base64_decode[0]; vp = vp;
 
-		struct {
-			int	d : 6;
-			int	c : 6;
-			int	b : 6;
-			int	a : 6;
-		} _64;
-	printf("_64 %d\n", sizeof(_64));
-
-	B64Chunk __;
-//	size_t b2	= sizeof(__.b2);
-	size_t b256	= sizeof(__.b256);
-	size_t b64	= sizeof(__.b64);
-	printf("b64 %d b256 %d\n", b64, b256);
-	assert(b256 == b64);
-
-	return 0;
-#if 0
 	Base64Encoder enc;
 
-	while(1) {
-		int i = getchar();
-		enc.Push(i);
+	while(!feof(stdin)) {
+		enc.Push(getchar());
 
-		int o;
-		while((o = enc.Pop()) != EOF) {
+		for(int o = enc.Pop(); o != EOF; o = enc.Pop()) {
 			printf("%c", o);
 		}
-		if(i == EOF)
-			break;
 	}
-	printf("\r\n");
 
 	return 0;
-#endif
-
-#if 0
-	do {
-		B64Chunk chunk;
-		int octets;
-		int	b64s;
-		int	i;
-
-		chunk.b256[0] = 0;
-		chunk.b256[1] = 0;
-		chunk.b256[2] = 0;
-
-		for(octets=0; octets<3; octets++) {
-			int c = getchar();
-			if(c == EOF) {
-				// pad the last b64 block with zero bits
-				chunk.b256[octets] = 0;
-				break;
-			}
-			chunk.b256[octets] = c;
-		}
-		if(octets == 0)
-			continue;
-
-		/* write enough b64s to include all the octets */
-		b64s = ((octets * 8) / 6) + (((octets * 8) % 6) ? 1 : 0);
-
-		/* print the four b64 encoded chars, perhaps with padding */
-		for(i = 0; i < 4; i++) {
-			if(i < b64s) {
-				char b = chunk.b64[i];
-				char e = base64_encode[b];
-				printf("%c", e);
-			} else {
-				printf("=");
-			}
-			llength++;
-			if(llength > maxlength) {
-				printf("\r\n");
-				llength = 0;
-			}
-		}
-
-	} while(!feof(stdin) && !ferror(stdin));
-
-	printf("\r\n");
-
-	return 0;
-#endif
 }
 
