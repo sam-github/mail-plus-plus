f064c2c23f930c678e337aefbb5b80b67099de26
Sam Roberts <vieuxtech@gmail.com>
2000-01-25 06:24:39 +0000

    Initial revision

---
+++ address.cc
@@ -0,0 +1,168 @@
+//
+// Mail address construction and parsing
+//
+
+#include <mail++/address.h>
+#include <mail++/parse822.h>
+
+//
+// MAddrSpec
+//
+
+MAddrSpec::MAddrSpec()
+{
+}
+MAddrSpec::MAddrSpec(const MMailBox& mailbox)
+{
+	*this = mailbox.AddrSpec();
+}
+MAddrSpec::MAddrSpec(Rope local_part, Rope domain) :
+	local_part_ (local_part), domain_ (domain)
+{
+	text_ += MQuoteLocalPart(local_part_);
+
+	// This isn't RFC compliant, but is a valid address on most
+	// systems.
+	if(!domain_.empty())
+	{
+		text_ += '@';
+		text_ += domain_;
+	}
+}
+MAddrSpec::Rope MAddrSpec::LocalPart() const
+{
+	return local_part_;
+}
+MAddrSpec::Rope MAddrSpec::Domain() const
+{
+	return domain_;
+}
+MAddrSpec::Rope MAddrSpec::Text() const
+{
+	return text_;
+}
+
+//
+// MMailBox
+//
+
+void MMailBox::Build()
+{
+	text_.clear();
+
+	if(!display_name_.empty())
+	{
+		text_ += display_name_;
+		text_ += " <";
+	}
+
+	text_ += addr_spec_.Text();
+
+	if(!display_name_.empty())
+	{
+		text_ += ">";
+	}
+}
+MMailBox::MMailBox()
+{
+}
+MMailBox::MMailBox(const MAddrSpec& addr_spec, Rope display_name) :
+	addr_spec_ (addr_spec), display_name_ (display_name)
+{
+	Build();
+}
+MMailBox::MMailBox(Rope local_part, Rope domain, Rope display_name) :
+	addr_spec_ (local_part, domain), display_name_ (display_name)
+{
+	Build();
+}
+const MAddrSpec& MMailBox::AddrSpec() const
+{
+	return addr_spec_;
+}
+MMailBox::Rope MMailBox::LocalPart() const
+{
+	return addr_spec_.LocalPart();
+}
+MMailBox::Rope MMailBox::Domain() const
+{
+	return addr_spec_.Domain();
+}
+MMailBox::Rope MMailBox::DisplayName() const
+{
+	return display_name_;
+}
+MMailBox::Rope MMailBox::Text() const
+{
+	return text_;
+}
+
+//
+// Helper Functions
+//
+
+// maybe should be public?
+template <class T>
+crope MakeList(const T& list)
+{
+	crope text;
+
+	T::const_iterator p = list.begin();
+	T::const_iterator e = list.end();
+
+	while(p != e)
+	{
+		if(p->Text().empty())
+			continue;
+
+		if(!text.empty())
+			text += ", ";
+
+		text += p->Text();
+
+		++p;
+	}
+	return text;
+}
+
+crope MAddressListToText(const MAddrSpecList& list)
+{
+	return MakeList(list);
+}
+crope MAddressListToText(const MMailBoxList& list)
+{
+	return MakeList(list);
+}
+
+crope MQuoteLocalPart(const crope& local_part)
+{
+	crope quoted = local_part;
+
+	MRfc822Tokenizer token;
+
+	int quoteit = 0;
+	for(
+		crope::iterator p = quoted.mutable_begin();
+		p != quoted.mutable_end();
+		++p)
+	{
+		if(*p == '.')
+			continue; // allowed in local-part
+
+		if(!token.IsAtomChar(*p))
+		{
+			quoteit = 1;
+			// some chars need quoting even in a string...
+			if(!token.IsSmtpQ(*p))
+				quoted.insert(p, '\\');
+		}
+	}
+	if(quoteit)
+	{
+		quoted.push_front('"');
+		quoted.push_back('"');
+	}
+
+	return quoted;
+}
+
---
+++ rfc821.h
@@ -0,0 +1,97 @@
+//
+//
+//
+
+#ifndef M_RFC_821_H
+#define M_RFC_821_H
+
+#include <ctype.h>
+
+#include <mail++/address.h>
+
+class MRfc821Tokenizer
+{
+public:
+	int IsC(char c) const
+	{
+		return isascii(c) && !IsSpecial(c) && c != ' ';
+	}
+	int IsSpecial(char c) const
+	{
+		if(strchr("<>()[]\\.,;:@\"", c))
+			return 1;
+
+		if(iscntrl(c))
+			return 1;
+
+		if(c == 127)
+			return 1;
+
+		return 0;
+	}
+};
+
+/**
+* Convert an RFC 822 mailbox to an RFC 821 (SMTP) "path", used as
+* the argument to the MAIL and RCPT commands, for instance. The
+* path syntax is defined in 4.1.2.
+*/
+crope MMailBoxToSmtpPath(const MMailBox& mailbox)
+{
+	// Relevant EBNF (in the syntax of RFC822, not RFC821):
+	//
+	// path = "<" mailbox ">"
+	// mailbox = local-part "@" domain
+	// local-part = dot-string / quoted-string
+	// dot-string = string *("." string)
+	// string = 1*char
+	// char = c / "\" x
+	// etc....
+
+	// Anyhow, the summary is that if any un-safe characters
+	// occur in the local-part, then the whole thing can be made
+	// a quoted string, and a few specials have to be quoted
+	// inside that string. There's other valid syntax, but we're
+	// making a path, not parsing one.
+
+	crope local_part = mailbox.LocalPart();
+	MRfc821Tokenizer token;
+	int	quoteit = 0;
+	for(
+		crope::iterator p = local_part.mutable_begin();
+		p != local_part.mutable_end();
+		++p)
+	{
+		if(*p == '.')
+			continue; // allowed in local-part
+
+		if(token.IsSpecial(*p) || *p == ' ')
+		{
+			quoteit = 1;
+			// some chars need quoting even in a string...
+			if(strchr("\r\n\"\\", *p))
+				local_part.insert(p, '\\');
+		}
+	}
+	if(quoteit)
+	{
+		local_part.push_front('"');
+		local_part.push_back('"');
+	}
+	// build the path
+	crope path;
+
+	path += '<';
+	path += local_part;
+	if(!mailbox.Domain().empty())
+	{
+		path += '@';
+		path += mailbox.Domain();
+	}
+	path += '>';
+
+	return path;
+};
+
+#endif
+
