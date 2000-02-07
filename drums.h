89401740e7ada6a38cd299e4b809a4024345417c
Sam Roberts <vieuxtech@gmail.com>
2000-02-07 05:12:00 +0000

    parsing new syntax, but some problems with CFWS in drums and grammer ambiguities with obsolete syntax

--- drums.h
+++ drums.h
@@ -13,23 +13,7 @@
 #include <ctype.h>
 
 /**
-* Core rules from Appendix A of RFC 2234.
-ALPHA          =  %x41-5A / %x61-7A   ; A-Z / a-z
-BIT            =  "0" / "1"
-CHAR           =  %x01-7F ; any 7-bit US-ASCII character, excluding NUL
-CR             =  %x0D ; carriage return
-CRLF           =  CR LF ; Internet standard newline
-CTL            =  %x00-1F / %x7F ; controls
-DIGIT          =  %x30-39 ; 0-9
-DQUOTE         =  %x22 ; " (Double Quote)
-HEXDIG         =  DIGIT / "A" / "B" / "C" / "D" / "E" / "F"
-HTAB           =  %x09 ; horizontal tab
-LF             =  %x0A ; linefeed
-LWSP           =  *(WSP / CRLF WSP) ; linear white space (past newline)
-OCTET          =  %x00-FF ; 8 bits of data
-SP             =  %x20 ; space
-VCHAR          =  %x21-7E ; visible (printing) characters
-WSP            =  SP / HTAB ; white space
+* Core rules from Appendix A of RFC 2234, ABNF.
 */
 class MCoreTokenizer
 {
@@ -37,36 +21,68 @@ private:
 	typedef crope::const_iterator Ptr;
 
 public:
-	int	IsDIGIT			(Ptr& p, const Ptr& e)
+	int	IsALPHA			(Ptr& p, const Ptr& e)
 	{
-		// DIGIT = %x30-39		; 0-9
+		// ALPHA =  %x41-5A / %x61-7A ; A-Z / a-z
 
 		if(p == e)
 			return 0;
 
-		if(*p < 0x30)
+		// Use the C library here, and maybe write our own version
+		// of these functions for all the following Is*() functions,
+		// they'll be faster than all these comparisons.
+
+		if(isalpha(*p)) {
+			++p;
+			return 1;
+		}
+		return 0;
+	}
+	// BIT =  "0" / "1"
+	// CHAR =  %x01-7F ; any 7-bit US-ASCII character, excluding NUL
+	// CR =  %x0D ; carriage return
+	// CRLF =  CR LF ; Internet standard newline
+	// CTL =  %x00-1F / %x7F ; controls
+
+	int	IsDIGIT			(Ptr& p, const Ptr& e)
+	{
+		// DIGIT = %x30-39 ; 0-9
+
+		if(p == e)
 			return 0;
 
-		if(*p > 0x39)
+		if(*p < '\x30')
+			return 0;
+
+		if(*p > '\x39')
 			return 0;
 
 		return 1;
 	}
+	// DQUOTE =  %x22 ; " (Double Quote)
+	// HEXDIG =  DIGIT / "A" / "B" / "C" / "D" / "E" / "F"
+
 	int IsHTAB(Ptr& p, const Ptr& e)
 	{
-		// HTAB = %x09			; horizontal tab
+		// HTAB =  %x09 ; horizontal tab
 
-		return p != e && *p == 0x09;
+		return p != e && *p == '\x09';
 	}
+	// LF =  %x0A ; linefeed
+	// LWSP =  *(WSP / CRLF WSP) ; linear white space (past newline)
+	// OCTET =  %x00-FF ; 8 bits of data
+
 	int IsSP(Ptr& p, const Ptr& e)
 	{
-		// SP = %x20
+		// SP =  %x20 ; space
 
 		return p != e && *p == 0x20;
 	}
+	// VCHAR =  %x21-7E ; visible (printing) characters
+
 	int	IsWSP			(Ptr& p, const Ptr& e)
 	{
-		// WSP = SP / HTAB		; white space
+		// WSP =  SP / HTAB ; white space
 
 		return IsSP(p, e) || IsHTAB(p, e);
 	}
@@ -92,7 +108,6 @@ public:
 		}
 		return i;
 	}
-
 	/**
 	* Utility function to get a char, if it exits.
 	*/
@@ -126,7 +141,6 @@ public:
 	MDrumsTokenizer(int strict = 0) : strict_(strict)
 	{
 	}
-#if 0
 	int Errors() const { return errors_; }
 
 	int UnterminatedQuotedString() const { return unterminated_qstr_; }
@@ -140,43 +154,13 @@ public:
 		unterminated_comment_ = 0;
 		invalid_chars_ = 0;
 	}
-
-	//
-	// Character Classification - could be rewritten in a C library
-	// independent way, my system's C library matches the RFC
-	// definitions, but I don't know if that's guaranteed.
-	//
-	int IsCHAR(char c) const;
-	int IsCTL(char c) const;
-	int IsSPACE(char c) const;
-	int IsHTAB(char c) const;
-	int IsLWSPChar(char c) const;
-	int IsSpecial(char c) const;
-	int IsAtomChar(char c) const;
-	int IsQText(char c) const;
-	int IsSmtpQ(char c) const;
-		// SMTP's version of qtext, called <q> in the RFC 821 syntax,
-		// also exludes <LF>
-
-	//
-	// Lexical Analysis - these tokens are all from RFC822,
-	// section 3.3, Lexical Tokens, though not all tokens are
-	// implemented.
-	//
-
-	int SkipWs			(Ptr& p, const Ptr& e);
-	int SkipComments	(Ptr& p, const Ptr& e);
-	int GetSpecial		(Ptr& p, const Ptr& e, char c);
-	int GetComment		(Ptr& p, const Ptr& e, crope& comment);
-	int GetAtom			(Ptr& p, const Ptr& e, crope& atom);
-	int GetQuotedPair	(Ptr& p, const Ptr& e, crope& qpair);
-	int GetQuotedString	(Ptr& p, const Ptr& e, crope& qstr);
-	int GetWord			(Ptr& p, const Ptr& e, crope& word);
-	int GetPhrase		(Ptr& p, const Ptr& e, crope& phrase);
-#endif
-
-	// DRUMS lexer elements
 	int GetFWS			(Ptr& p, const Ptr& e)
+	{
+		crope fws;
+
+		return GetFWS(p, e, fws);
+	}
+	int GetFWS			(Ptr& p, const Ptr& e, crope& fws)
 	{
 		// FWS = ([*WSP CRLF] 1*WSP) / obs-FWS
 		//
@@ -190,16 +174,197 @@ public:
 			wsp = GetWSP(p, e); 
 		}
 
-		if(wsp == 0)
+		if(wsp == 0) {
 			p = save;
+			return 0;
+		}
 
-		return wsp > 0;
+		fws.append(' ');
+
+		return 1;
+	}
+		// NO-WS-CTL = %d1-8 / %d11 / %d12 / %d14-31 / %d127
+		//      ; ctl chars minus the CR, LF, and WSP
+	int GetCText		(Ptr& p, const Ptr& e, crope& ctext)
+	{
+		// ctext = NO-WS-CTL /		; Non white space controls
+		//         %d33-39 /		; The rest of the US-ASCII
+		//         %d42-91 /		; chars, except "(", ")",
+		//         %d93-126			; or "\".
+
+		if(p == e)
+			return 0;
+
+		// do this by searching for disallowed charcters:
+		char* notok = "\x009\x00A\x00D\x020()\\";
+		char c = *p;
+
+		if(c == '\0' || strchr(notok, c))
+			return 0;
+
+		ctext.append(c);
+		++p;
+
+		return 1;
+	}
+	int GetText			(Ptr& p, const Ptr& e, crope& text)
+	{
+		// text = %d1-9 /		; characters excluding CR and LF
+		//        %d11-12 /
+		//        %d14-127 /
+		//        obs-text
+
+		if(p == e)
+			return 0;
+
+		Ptr save = p;
+
+		char c = *p;
+
+		++p;
+
+		if(c >= 1 && c <= 9) {
+			text.append(c);
+			return 1;
+		}
+		if(c >= 11 && c <= 12) {
+			text.append(c);
+			return 1;
+		}
+		if(c >= 14 && c <= 127) {
+			text.append(c);
+			return 1;
+		}
+
+		p = save;
+	
+		return GetObsText(p, e, text);
+	}
+	int GetQuotedPair	(Ptr& p, const Ptr& e, crope& qp)
+	{
+		// quoted-pair = ("\" text) / obs-qp
+
+		Ptr save = p;
+
+		if(GetChar(p, e, '\\') && GetText(p, e, qp))
+			return 1;
+
+		save = p;
+
+		return 0;
+	}
+	int GetCContent		(Ptr& p, const Ptr& e, crope& cc)
+	{
+		// ccontent = ctext / quoted-pair / comment
+
+		return
+			GetCText(p, e, cc) ||
+			GetQuotedPair(p, e, cc) ||
+			GetComment(p, e, cc);
+	}
+	int GetComment		(Ptr& p, const Ptr& e)
+	{
+		crope comment;
+
+		return GetComment(p, e, comment);
+	}
+	int GetComment		(Ptr& p, const Ptr& e, crope& comment)
+	{
+		// comment = "(" *([FWS] ccontent) [FWS] ")"
+		//
+		// --> "(" *(FWS / ccontent) ")"
+
+		Ptr save = p;
+
+		crope c;
+
+		if(!GetChar(p, e, '('))
+			return 0;
+
+		while(GetFWS(p, e, c) || GetCContent(p, e, c))
+			;
+
+		if(!GetChar(p, e, ')')) {
+			p = save;
+			return 0;
+		}
+
+		comment = c;
+
+		return 1;
 	}
 	int GetCFWS			(Ptr& p, const Ptr& e)
 	{
 		// CFWS = *([FWS] comment) (([FWS] comment) / FWS)
+		//
+		// --> 1*(FWS / comment)
 
-		return 1;
+		int count = 0;
+
+		while(GetFWS(p, e) || GetComment(p, e))
+			++count;
+
+		if(!strict_)
+			return 1;
+
+		return count;
+	}
+
+	// Obsolete Syntax
+	int GetObsText			(Ptr& p, const Ptr& e, crope& text)
+	{
+		// obs-text = *LF *CR *(obs-char *LF *CR)
+
+		int count = 0;
+
+		while(GetChar(p, e, '\n')) {
+			++count;
+			text.append('\n');
+		}
+
+		while(GetChar(p, e, '\r')) {
+			++count;
+			text.append('\r');
+		}
+
+		for(;;)
+		{
+			if(!GetObsChar(p, e, text))
+				return count;
+			
+			while(GetChar(p, e, '\n')) {
+				++count;
+				text.append('\n');
+			}
+
+			while(GetChar(p, e, '\r')) {
+				++count;
+				text.append('\r');
+			}
+		}
+	}
+	int GetObsChar		(Ptr& p, const Ptr& e, crope& text)
+	{
+		// obs-char = %d0-9 /		; %d0-127 excluding CR and LF
+		//        %d11 /
+		//        %d12 /
+		//        %d14-127
+
+		if(p == e)
+			return 0;
+
+		char c = *p;
+
+		if(c == '\r' || c == '\n')
+			return 0;
+		
+		if(c > 0 && c <= 127) {
+			text.append(c);
+			++p;
+			return 1;
+		}
+	
+		return 0;
 	}
 	/**
 	* Get's at least min, and at most max digit characters, where max
@@ -226,6 +391,42 @@ public:
 			digits = accum;
 			return i;
 		}
+		p = save;
+
+		return 0;
+	}
+
+protected:
+	int GetString		(Ptr& p, const Ptr& e, const char* n, crope& name)
+	{
+		const char* names[] = {
+			0,
+			0
+		};
+		names[0] = n;
+
+		return GetEnumString(p, e, names, name);
+	}
+	int GetEnumString	(Ptr& p, const Ptr& e, const char* names[], crope& name)
+	{
+		if(p == e)
+			return 0;
+
+		Ptr save = p;
+
+		crope t(p, e);
+		const char* text = t.c_str();
+
+		for(int j = 0; names[j]; ++j) {
+			int l = strlen(names[j]);
+
+			if(strnicmp(text, names[j], l) == 0) {
+				p += l;
+				name = names[j];
+				return 1;
+			}
+		}
+
 		return 0;
 	}
 };
@@ -237,12 +438,9 @@ class MDateTimeParser : public MDrumsTokenizer
 private:
 	typedef crope::const_iterator Ptr;
 
-	MDrumsTokenizer& lexer;
-
 public:
 	MDateTimeParser(int strict = 0) :
-		MDrumsTokenizer(strict),
-		lexer(*this)
+		MDrumsTokenizer(strict)
 	{
 	}
 	int DateTime		(const crope& text, MDateTime& date_time)
@@ -256,32 +454,42 @@ public:
 	{
 		// date-time = [ day-of-week "," ] date FWS time [CFWS]
 
+		Ptr save = p;
+
 		crope day_name;
 
 		if(GetDayOfWeek(p, e, day_name)) {
-			if(!lexer.GetChar(p, e, ','))
+			if(!GetChar(p, e, ',')) {
+				p = save;
 				return 0;
+			}
 		}
 
 		int		day;
 		crope	month;
 		int		year;
 
-		if(!GetDate(p, e, day, month, year))
+		if(!GetDate(p, e, day, month, year)) {
+			p = save;
 			return 0;
+		}
 
-		if(!lexer.GetFWS(p, e))
-			return 0;
+		if(!GetFWS(p, e)) {
+//			p = save;
+//			return 0;
+		}
 
 		int hour;
 		int	min;
 		int	sec;
 		int	zone;
 
-		if(!GetTime(p, e, hour, min, sec, zone))
+		if(!GetTime(p, e, hour, min, sec, zone)) {
+			p = save;
 			return 0;
+		}
 
-		lexer.GetCFWS(p, e);
+		GetCFWS(p, e);
 
 		// should I confirm that p == e?
 
@@ -295,7 +503,7 @@ public:
 
 		Ptr save = p;
 
-		lexer.GetFWS(p, e);
+		GetFWS(p, e);
 
 		if(GetDayName(p, e, day_name))
 			return 1;
@@ -324,7 +532,7 @@ public:
 			0
 		};
 
-		return GetName(p, e, days, day_name);
+		return GetEnumString(p, e, days, day_name);
 	}
 	int GetDate			(Ptr& p, const Ptr& e, int& day, crope& month, int& year)
 	{
@@ -360,8 +568,8 @@ public:
 
 		crope digits;
 
-		lexer.GetFWS(p, e);
-		if(lexer.GetDigits(p, e, 1, 2, digits)) {
+		GetFWS(p, e);
+		if(GetDigits(p, e, 1, 2, digits)) {
 			day = atoi(digits.c_str());
 			return 1;
 		}
@@ -380,9 +588,9 @@ public:
 
 		Ptr save = p;
 
-		if(lexer.GetFWS(p, e) && GetMonthName(p, e, m) && lexer.GetFWS(p, e)) {
-			month = m;
-			return 1;
+		if(GetFWS(p, e) && GetMonthName(p, e, m) && GetFWS(p, e)) {
+//			month = m;
+//			return 1;
 		}
 		p = save;
 
@@ -414,7 +622,7 @@ public:
 			0
 		};
 
-		return GetName(p, e, month_names, month);
+		return GetEnumString(p, e, month_names, month);
 	}
 	int GetYear			(Ptr& p, const Ptr& e, int& year)
 	{
@@ -422,7 +630,7 @@ public:
 
 		crope digits;
 
-		if(lexer.GetDigits(p, e, 4, 0, digits)) {
+		if(GetDigits(p, e, 4, 0, digits)) {
 			year = atoi(digits.c_str());
 			return 1;
 		}
@@ -443,8 +651,8 @@ public:
 		if(!GetTimeOfDay(p, e, h, m, s)) {
 			p = save; return 0;
 		}
-		if(!lexer.GetFWS(p, e)) {
-			p = save; return 0;
+		if(!GetFWS(p, e)) {
+//			p = save; return 0;
 		}
 		if(!GetZone(p, e, z)) {
 			p = save; return 0;
@@ -470,14 +678,14 @@ public:
 		if(!GetHMS(p, e, h)) {
 			p = save; return 0;
 		}
-		if(!lexer.GetChar(p, e, ':')) {
+		if(!GetChar(p, e, ':')) {
 			p = save; return 0;
 		}
 		if(!GetHMS(p, e, m)) {
 			p = save; return 0;
 		}
 
-		if(lexer.GetChar(p, e, ':')) {
+		if(GetChar(p, e, ':')) {
 			if(!GetHMS(p, e, s)) {
 				p = save; return 0;
 			}
@@ -497,14 +705,20 @@ public:
 		// minute = 2DIGIT / obs-minute
 		// sec    = 2DIGIT / obs-second
 
+		if(GetObsHMS(p, e, hms))
+			return 1;
+
 		crope digits;
 
-		if(lexer.GetDigits(p, e, 2, 2, digits)) {
+		// Because of the CFWS amibiguity, re-order these rules to
+		// try and match the most liberal first.
+
+		if(GetDigits(p, e, 2, 2, digits)) {
 			hms = atoi(digits.c_str());
 			return 1;
 		}
 
-		return GetObsHMS(p, e, hms);
+		return 0;
 	}
 	int GetZone			(Ptr& p, const Ptr& e, int& zone)
 	{
@@ -523,7 +737,7 @@ public:
 
 		crope digits;
 
-		if(lexer.GetDigits(p, e, 4, 4, digits)) {
+		if(GetDigits(p, e, 4, 4, digits)) {
 			zone = atoi(digits.c_str());
 
 			int h = zone / 100;
@@ -548,55 +762,164 @@ public:
 
 	int	GetObsDayOfWeek	(Ptr& p, const Ptr& e, crope& day_name)
 	{
-		return 0;
+		// obs-day-of-week = [CFWS] day-name [CFWS]
+
+		GetCFWS(p, e);
+
+		int ok = GetDayName(p, e, day_name);
+
+		GetCFWS(p, e);
+
+		return ok;
 	}
 	int GetObsMonth		(Ptr& p, const Ptr& e, crope& month)
 	{
+		// obs-month = CFWS month-name CFWS
+
+		Ptr save = p;
+
+		if(GetCFWS(p, e) && GetMonthName(p, e, month) && GetCFWS(p, e))
+			return 1;
+
+		p = save;
+
 		return 0;
 	}
 	int GetObsYear		(Ptr& p, const Ptr& e, int& year)
 	{
-		return 0;
+		// obs-year = [CFWS] 2*DIGIT [CFWS]
+
+		Ptr save = p;
+
+		GetCFWS(p, e);
+
+		crope digits;
+
+		if(!GetDigits(p, e, 2, 0, digits)) {
+			p = save;
+			return 0;
+		}
+
+		// I didn't make this up, its from DRUMS 4.3, Obsolete Date
+		// and Time
+
+		int y = atoi(digits.c_str());
+
+		if(digits.size() == 2 && y <= 49) {
+			year = 2000 + y;
+		} else if(digits.size() == 2 || digits.size() == 3) {
+			year = 1900 + y;
+		} else {
+			year = y;
+		}
+
+		GetCFWS(p, e);
+
+		return 1;
 	}
 	int GetObsDay		(Ptr& p, const Ptr& e, int& day)
 	{
-		return 0;
-	}
-	int GetObsHMS		(Ptr& p, const Ptr& e, int& day)
-	{
-		// obs-hour =
-		// obs-minute =
-		// obs-second =
+		// obs-day = [CFWS] 1*2DIGIT [CFWS]
 
-		return 0;
+		Ptr save = p;
+
+		GetCFWS(p, e);
+
+		crope digits;
+
+		if(!GetDigits(p, e, 1, 2, digits)) {
+			p = save;
+			return 0;
+		}
+
+		day = atoi(digits.c_str());
+
+		GetCFWS(p, e);
+
+		return 1;
+	}
+	int GetObsHMS		(Ptr& p, const Ptr& e, int& hms)
+	{
+		// obs-hour = [CFWS] 2DIGIT [CFWS]
+		// obs-minute = [CFWS] 2DIGIT [CFWS]
+		// obs-second = [CFWS] 2DIGIT [CFWS]
+
+		Ptr save = p;
+
+		GetCFWS(p, e);
+
+		crope digits;
+
+		if(!GetDigits(p, e, 2, 2, digits)) {
+			p = save;
+			return 0;
+		}
+
+		hms = atoi(digits.c_str());
+
+		GetCFWS(p, e);
+
+		return 1;
 	}
 	int GetObsZone		(Ptr& p, const Ptr& e, int& zone)
 	{
-		return 0;
-	}
+		// obs-zone = "UT" / "GMT" /	; Universal Time
+		//
+		//								; North American UT offsets
+		//            "EST" / "EDT" /	; Eastern: -5 / -4
+		//            "CST" / "CDT" /	; Central: -6 / -5
+		//            "MST" / "MDT" /	; Mountain: -7 / -6
+		//            "PST" / "PDT" /	; Pacific: -8 / -7
+		//
+		//            %d65-73 /			; Military zones "A" - "I"
+		//            %d75-90 /			; and "K" through "Z", either
+		//            %d97-105 /		; case.
+		//            %d107-122
 
-private:
-	int GetName			(Ptr& p, const Ptr& e, const char* names[], crope& name)
-	{
-		Ptr save = p;
+		if(p == e)
+			return 0;
 
-		char str[] = "123";
-		for(int i = 0; i < 3; ++i) {
-			if(p != e) {
-				str[i] = *p;
-				++p;
-			} else {
-				str[i] = '\0';
-			}
-		}
+		struct TZMap
+		{
+			char*	name;
+			int		offset;
+		};
 
-		for(int j = 0; names[j]; ++j) {
-			if(stricmp(str, names[j]) == 0) {
-				name = str;
+		static TZMap tzs[] = {
+			{ "UT",		0 },
+			{ "GMT",	0 },
+			{ "EST",	-5 },
+			{ "EDT",	-4 },
+			{ "CST",	-6 },
+			{ "CDT",	-5 },
+			{ "MST",	-7 },
+			{ "MDT",	-6 },
+			{ "PST",	-8 },
+			{ "PDT",	-7 },
+			0
+		};
+
+		crope name;
+
+		for(int i = 0; tzs[i].name; ++i) {
+			if(GetString(p, e, tzs[i].name, name)) {
+				zone = tzs[i].offset;
 				return 1;
 			}
 		}
-		p = save;
+
+		// Any single ALPHA except "J" is so-called military time.
+		// It is to be interpreted as "unknown" or UTC time.
+		if(IsALPHA(p, e)) {
+			if(*p != 'J' && *p != 'j') {
+				++p;
+				zone = 0;
+				return 1;
+			}
+		}
+
+		// DRUMS suggests that other alphabetic time zones SHOULD be
+		// parsed and interpreted as UTC. 
 
 		return 0;
 	}
--- test/t_drums.cc
+++ test/t_drums.cc
@@ -1,35 +1,62 @@
 //
+// Tests the DRUMS parser.
 //
+// $Log$
+// Revision 1.2  2000/02/07 05:12:00  sam
+// parsing new syntax, but some problems with CFWS in drums
+// and grammer ambiguities with obsolete syntax
 //
 
 #include <mail++/drums.h>
 
+#include <iostream.h>
+#include <iomanip.h>
+
 void t_datetime()
 {
 	const char* dts[] = {
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Tue,  1 Jul 2003 10:52:37 +0200",
-		"Thu, 13 Feb 1969 23:32:54 -0330",
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Fri, 21 Nov 1997 10:01:10 -0600",
-		"Fri, 21 Nov 1997 11:00:00 -0600",
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Mon, 24 Nov 1997 14:22:01 -0800",
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Fri, 21 Nov 1997 09:55:06 -0600",
-		"Tue,  1 Jul 2003 10:52:37 +0200",
-		"     21 Nov 97 09:55:06 GMT",
-		"Fri, 21 Nov 1997 09(comment):   55  :  06 -0600",
-		"     21 Nov 1997 10:05:43 -0600",
-		"     21 Nov 1997 10:01:22 -0600",
+
+		"Fri,(c) 21 Nov 1997 09:55:06 -0600",
+
 		"Thu,\r\n"
 		"    13\r\n"
 		"       Feb\r\n"
 		"          1969\r\n"
 		"                23:32\r\n"
-		"             -0330 (Newfoundland Time)",
-		"Fri, 21 Nov 1997 09(comment):   55  :  06 -0600",
+		"      €       -0330 (Newfoundland Time)",
+
+		"Fri , 21 Nov 1997 09():55:06 -0600",
+		"Fri(), 21 Nov 1997 09():55:06 -0600",
+		"Fri (), 21 Nov 1997 09():55:06 -0600",
+		"Fri() , 21 Nov 1997 09():55:06 -0600",
+
+		"Fri,()21 Nov 1997 09():55:06 -0600",
+		"Fri, ()21 Nov 1997 09():55:06 -0600",
+		"Fri, () 21 Nov 1997 09():55:06 -0600",
+		"Fri,() 21 Nov 1997 09():55:06 -0600",
+
+		"Fri, 21()Nov 1997 09():55:06 -0600",
+		"Fri, 21 ()Nov 1997 09():55:06 -0600",
+		"Fri, 21 () Nov 1997 09():55:06 -0600",
+		"Fri, 21() Nov 1997 09():55:06 -0600",
+
+		"21 Nov 97 13:40:65 -0600",
+		"Fri, 21 Nov 1997 09():55:06 -0600",
+		"Fri, 21 Nov 1997 09 :   55  :  06 -0600",
+		"Fri, 21 Nov 1997 09 (comment) :   55  :  06 -0600",
+		"Fri, 21 Nov 1997 ()09 :55:06 -0600",
+		"Fri, 21 Nov 1997 09:   55:06 -0600",
+		"(hello!) Fri, 21 Nov 1997(comment)09:55:06 -0600",
+		"21 Nov 97 09:55:06 GMT",
+		"21 Nov 97 13:40:65 -0600",
+		"Fri, 21 Nov 1997 10:01:10 -0600",
+		"Tue, 1 Jul 20003 10:52:37 +0200",
+		"Thu, 13 Feb 48 23:32 -0330",
+		"Thu, 13 Feb 49 23:32 -0330",
+		"Thu, 13 Feb 50 23:32 -0330",
+		"Fri, 21 Nov 1997 09:55 -0600",
+		"Tue, 1 Jul 2063 10:52:37 +0200",
+		"Fri, 21 Nov 1997 09:55:06 -0600",
 		0
 	};
 
@@ -43,15 +70,27 @@ void t_datetime()
 
 		int ok = parser.DateTime(text, dt);
 
-		cout
-			<< setw(2) << i << ": "
-			<< dts[i] << " -> " << dt.Text() << endl;
+		cout << setw(2) << i << ": " << dts[i] << "\t-> ";
+
+		if(ok) {
+			cout << dt.Text() << endl;
+		} else {
+			cout << "failed!" << endl;
+		}
 	}
+}
+
+void t_localtime()
+{
+	MDateTime dt(time(0));
+
+	cout << "localtime -> " << dt.Text() << endl;
 }
 
 int main()
 {
-	t_datetime();
+	//t_datetime();
+	t_localtime();
 
 	return 0;
 }
