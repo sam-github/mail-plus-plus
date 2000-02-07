6c85f6fd2333a960be2b83f11b35e3d21ee7c8fd
Sam Roberts <vieuxtech@gmail.com>
2000-02-07 05:15:18 +0000

    converts to text, and make from a time_t

--- datetime.h
+++ datetime.h
@@ -5,9 +5,6 @@
 #ifndef M_DATETIME_H
 #define M_DATETIME_H
 
-#include <iomanip.h>
-#include <strstream.h>
-
 #include <mail++/rope.h>
 
 #include <time.h>
@@ -17,6 +14,7 @@ class MDateTime
 private:
 	typedef const crope& Rope;
 
+	// the time fields used by mail
 	crope	dow;
 
 	int		day;
@@ -28,71 +26,31 @@ private:
 	int		sec;
 
 	int		zone;
+	crope	zonename;
 
+	// the local time info
+	time_t	utc_;
+
+	// the time in mail format
 	crope text_;
 
-	void Build()
-	{
-		strstream ss;
-
-		if(!dow.empty()) {
-			ss
-				<< ""
-				<< dow
-				<< ", ";
-		}
-
-		ss << setw(2) << day << " " << month << " " << setw(4) << year << " ";
-		ss << setfill('0');
-		ss << setw(2) << hour << ":";
-		ss << setw(2) << minute << ":";
-		ss << setw(2) << sec << " ";
-
-		// convert zone in minutes to hhmm
-		int z = (zone / 60) * 100 + (zone % 60);
-		ss << setiosflags(ios::showpos|ios::internal) << setw(5) << z;
-
-		ss << '\0';
-
-		text_.clear();
-
-		const char* t = ss.str();
-
-		text_ = t;
-	}
+	void Build();
+	void ToTime();
+	void FromTime();
 
 public:
-	MDateTime() :
-			dow		(""),
-			day		(0),
-			month	(""),
-			year	(0),
-			hour	(0),
-			minute	(0),
-			sec		(0),
-			zone	(0)
-	{
-	}
+	MDateTime();
 	MDateTime(
 		const crope& dow,
 		int day, const crope& month, int year,
 		int hour, int minute, int sec,
 		int zone
-		) :
-			dow		(dow),
-			day		(day),
-			month	(month),
-			year	(year),
-			hour	(hour),
-			minute	(minute),
-			sec		(sec),
-			zone	(zone)
-	{
-		Build();
-	}
+		);
 	MDateTime(time_t utc);
 
-	Rope Text() const { return text_; }
+	time_t Time();
+
+	Rope Text() const;
 };
 
 #endif
