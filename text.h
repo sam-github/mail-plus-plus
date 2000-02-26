d031266ce5617bc195534da5b52ce9628280fc2e
Sam Roberts <vieuxtech@gmail.com>
2000-02-26 18:35:41 +0000

    implemented a mime text media type wrapper

--- text.h
+++ text.h
@@ -1,19 +1,50 @@
 //
-//
+// Mime Text Type
 //
 
-#ifndef M_ISTEXT_H
-#define M_ISTEXT_H
+#ifndef M_TEXT_H
+#define M_TEXT_H
 
 #include <mail++/rope.h>
+#include <mail++/mime.h>
+
+class MText
+{
+private:
+	typedef crope Rope;
+	typedef Rope::const_iterator Ptr;
+
+	static const Rope type_;
+
+	Rope    subtype_;
+	Rope	charset_;
+
+	Rope	text_;
+
+	Rope	encoded_text_;
+	MEncoding	encoding_;
+
+public:
+	MText(const Rope& subtype = "plain");
+	MText(const Rope& subtype, const Rope& text);
+
+		// accessors
+	const Rope&		Type() const;
+	const Rope&		SubType() const;
+	const Rope&		Text() const;
+	MMessage&		Fill(MMessage& mail) const;
+
+		// modifiers
+	const Rope&		Text(const Rope& text);
+};
 
 /**
 A file is Unix (lines seperated by a single NL (12)) text if:
 - all lines are composed of printable ASCII (32 - 126), or TAB (11),
-- no lines are over a particular maximum length.
+- no lines (not including the NL) are over a particular maximum length.
 */
 template <class T>
-int IsUnixText(T p, const T& e, int maxll)
+inline int IsUnixText(T p, const T& e, int maxll)
 {
 	int ll = 0;
 
@@ -43,7 +74,7 @@ int IsUnixText(T p, const T& e, int maxll)
 	return 1;
 }
 
-int IsUnixText(const crope& text, int maxll = 120)
+inline int IsUnixText(const crope& text, int maxll = 120)
 {
 	return IsUnixText(text.begin(), text.end(), maxll);
 }
