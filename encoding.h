1e88d5110fd2992c0ca6cebf58dbd82ceef5d3d4
Sam Roberts <vieuxtech@gmail.com>
2000-02-26 18:24:39 +0000

    added encoding for QP and B64

--- encoding.h
+++ encoding.h
@@ -5,6 +5,29 @@
 #ifndef M_ENCODING_H
 #define M_ENCODING_H
 
+#include <mail++/rope.h>
+#include <mail++/base64.h>
+
+inline crope Base64Encode(const crope& in, int ll = Base64::RFCLL)
+{
+	crope out;
+
+	Base64::Encoder encoder(ll);
+
+	for(crope::const_iterator p = in.begin(); p != in.end(); ++p)
+	{
+		encoder.Push(*p);
+		for(int o = encoder.Pop(); o != EOF; o = encoder.Pop()) {
+			out.push_back(char(o));
+		}
+	}
+	return out;
+}
+inline crope QuotedPrintableEncode(const crope& in, int ll = Base64::RFCLL)
+{
+	return in;
+}
+
 /**
 7bit - max line length is 998 octets between CRLF pairs
      - no nulls
@@ -17,6 +40,9 @@ Note that Unix text will be binary, since it has bare NLs in it.
 */
 class MDataType
 {
+public:
+	enum EDataType { E_7BIT, E_8BIT, E_BINARY };
+
 private:
 	typedef crope Rope;
 	typedef Rope::const_iterator Ptr;
@@ -30,12 +56,11 @@ private:
 	int 	maxll_;
 	int		qpok_;
 
-	EDataType	type;
+	EDataType	type_;
 
 public:
-	enum EDataType { E_7BIT, E_8BIT, E_BINARY };
 
-	MDataType(Ptr p, const Ptr& e) :
+	MDataType(Ptr p, const Ptr& e, int maxll = 998) :
 		eightbit_	(0),
 		null_		(0),
 		nl_			(0),
@@ -50,22 +75,22 @@ public:
 		{
 			int c = *p;
 
-			octets++;
+			octets_++;
 			ll++;
 
 			if(c > 127) {
-				eightbit++;
+				eightbit_++;
 			} else if(c == 0) {
-				null++;
+				null_++;
 			} else if(c == '\n') {
-				nl++;
+				nl_++;
 			} else if(c == '\r') {
 				// followed by a NL it's a crlf, else it's just a cr
 				Ptr n = p + 1;
 				if(n != e && *n == '\n') {
 					p = n;
-					crlf++;
-					octets++;
+					crlf_++;
+					octets_++;
 					ll++;
 
 					if(ll > maxll)
@@ -73,21 +98,21 @@ public:
 
 					ll = 0;
 				} else {
-					cr++;
+					cr_++;
 				}
 			}
 
-			if((c >= 33 && c <= 60) || ((c >= 62 && c <= 126))
+			if((c >= 33 && c <= 60) || (c >= 62 && c <= 126))
 				qpok_++;
 		}
 
-		type = E_BINARY; // unless we prove otherwise
+		type_ = E_BINARY; // unless we prove otherwise
 
 		if(maxll <= 998 && !null_ && !cr_ && !nl_) {
-			type = E_8BIT;
+			type_ = E_8BIT;
 
 			if(!eightbit_)
-				type = E_7BIT;
+				type_ = E_7BIT;
 		}
 	}
 
@@ -100,15 +125,44 @@ public:
 		// The aproximate size of a quoted-printable encoding, ignoring
 		// breaking of long lines and quoting of '=' signs.
 
-		int qpsize = qpok_ + (octets_ - qpok_ - crlf) * 3;
+		int qpsize = qpok_ + (octets_ - qpok_ - crlf_) * 3;
 		int b64size = (octets_ / 3) * 4;
 
 		switch(type_)
 		{
 		case E_BINARY:
+			return MEncoding::EBase64;
+		case E_8BIT:
+			return MEncoding::EQuotedPrintable;
+/*
+		case E_BINARY:
+		case E_8BIT:
 			if(qpsize < ((b64size * 9) / 10))
-
+				return MEncoding::EQuotedPrintable;
+			else
+				return MEncoding::EBase64;
+			break;
+*/
+		case E_7BIT:
+			return MEncoding::E7Bit;
 		}
+		// never reached, only for compiler complaints
+		return MEncoding::E7Bit;
+	}
+	static const MEncoding& Encode(crope& out, const crope& in, int ll = Base64::RFCLL)
+	{
+		MDataType dt(in.begin(), in.end());
+
+		const MEncoding& encoding = dt.UseEncoding();
+
+		if(encoding == MEncoding::E7Bit) {
+			out = in;
+		} else if(encoding == MEncoding::EQuotedPrintable) {
+			out = QuotedPrintableEncode(in, ll);
+		} else if(encoding == MEncoding::EBase64) {
+			out = Base64Encode(in, ll);
+		}
+		return encoding;
 	}
 };
 
